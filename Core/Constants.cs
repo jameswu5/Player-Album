59142ac5a4e2aadcb376578ac58305ea67358bd2
James Wu <jameswu144@gmail.com>
2023-12-27 12:10:39 +0000

    branch

--- Core/Constants.cs
+++ Core/Constants.cs
@@ -5,7 +5,7 @@ namespace PlayerAlbum;
 public static class Constants {
     public const int PlayersPerPack = 5;
 
-    public const string SavePath = "save/save.txt";
+    public const string SavePath = "save/test.txt";
     public const string FacePathRoot = "static/faces/";
     public const string BadgePathRoot = "static/badges/";
     public const string DatabasePath = "database/players.db";
